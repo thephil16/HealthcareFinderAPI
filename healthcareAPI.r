275fe51f1666526beaa8c51964b2dcdfc43daedb
PGA <philipa@ufl.edu>
2017-06-24 17:27:03 -0400

    Add initial fields to R script to be filled out for classes

---
+++ healthcareAPI.r
@@ -0,0 +1,13 @@
+require(R6)
+require(httr)
+require(xml2)
+
+healthcareFinderRequest <- R6Class()
+
+zipcodeValidation <- R6Class()
+
+IFP_PlanFinder <- R6Class()
+
+IFP_PlanDetails <- R6Class()
+
+healthcareFinderResponse <- R6Class()
