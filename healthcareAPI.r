8d8eccc39f65219e389893d71b08bfa0ed822c40
PGA <philipa@ufl.edu>
2017-06-24 23:26:41 -0400

    Started work on healthcare finder request R6 superclass

--- healthcareAPI.r
+++ healthcareAPI.r
@@ -2,12 +2,39 @@ require(R6)
 require(httr)
 require(xml2)
 
-healthcareFinderRequest <- R6Class()
+healthcareFinderRequest <- R6Class("HealthcareFinderRequest",
+                                   public = list(
+                                     initialize = function(query, urlSuffix){
+                                       # validate input
+                                       private$requestXML <- query
+                                       private$url <- paste("https://api.finder.healthcare.gov", "v3.0", urlSuffix, sep = "/")
+                                       private$header <- add_headers()
+                                     },
+                                     
+                                     getQueryXML = function(){
+                                       return(private$requestXML)
+                                     }
+                                     
+                                     getHeader = function(){
+                                       return(private$header)
+                                     }
+                                     
+                                     getURL = function(){
+                                       return(private$url)
+                                     }
+                                   ),
+                                   
+                                   private = list(
+                                     requestXML = "",
+                                     url = "",
+                                     header = ""
+                                   )
+)
 
-zipcodeValidation <- R6Class()
+zipcodeValidation <- R6Class("ZipcodeValidation")
 
-IFP_PlanFinder <- R6Class()
+IFP_PlanFinder <- R6Class("IFPPlanFinder")
 
-IFP_PlanDetails <- R6Class()
+IFP_PlanDetails <- R6Class("IFPPlanDetails")
 
-healthcareFinderResponse <- R6Class()
+healthcareFinderResponse <- R6Class("HealthcareFinderResponse")
