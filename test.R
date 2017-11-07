cea0697f6f1f0ca700d6c237940e98bad40454a7
Philip Allen <philip.allen@avmed.org>
2017-11-07 10:50:41 -0500

    Several Updates
    Updated capability to pull Individual Details
    Corrected date formatting so that correct date was sent to API when pulling quote data.
    Added a corrected schema file that can be used (at least in Python) to validate XML requests

--- test.R
+++ test.R
@@ -1,9 +1,11 @@
-defaultDOB <- ymd(20170801) - (30 * years())
+defaultDOB <- ymd(20180101) - (30 * years())
 defaultMbr <- enrollee$new(defaultDOB, "Male", NULL, "SELF", TRUE)
 mbrList <- list(defaultMbr)
-detailRequest <- IFP_PlanDetails$new(mbrList, "32003", "CLAY", ymd(20170801), "16842FL0120044")
+detailRequest <- IFP_PlanDetails$new(mbrList, "32003", "CLAY", ymd(20180101), "16842FL0120044")
 detailResponse <- HealthcareAPIRequest(detailRequest)
 detailResults <- processAPIResponse(detailResponse)
+
+quoteRequest <- IFP_PlanQuote$new(mbrList, "32605", "ALACHUA", ymd("20180101"))
 
 detailRequest2 <- IFP_PlanDetails$new(mbrList, "32003", "CLAY", ymd(20170801), "30252FL0020076")
 detailResponse2 <- HealthcareAPIRequest(detailRequest2)
@@ -20,3 +22,18 @@ detailResults4 <- processAPIResponse(detailResponse4)
 detailRequest5 <- defaultIFPDetailsRequest(ymd(20170801),  "32003", "CLAY", "19898FL0230054")
 detailResponse5 <- HealthcareAPIRequest(detailRequest5)
 detailResults5 <- processAPIResponse(detailResponse5)
+
+testFunction <- function(enrollees){
+  for(i in enrollees){
+    if(!any(class(i) == "Enrollee")){
+      stop("All enrollees must be in 'Enrollee' class!")
+    }
+    if(i$getRelation() == "SELF"){
+      selfCount <- selfCount + 1
+    } else if(i$getRelation() == "SPOUSE" | i$getRelation() == "LIFE_PARTNER"){
+      spouseCount <- spouseCount + 1
+    } else {
+      dependentCount <- dependentCount + 1
+    }
+  }
+}
